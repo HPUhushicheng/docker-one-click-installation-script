e6c2e92c2d76914d7e543b1514c4ad9c33543419
hushicheng <120257354+HPUhushicheng@users.noreply.github.com>
2024-04-20 19:19:37 +0800

    Update zhumao.sh

--- zhumao.sh
+++ zhumao.sh
@@ -301,9 +301,10 @@ updateSystem() {
 installDocker(){
     echoContent yellow " ---> 安装docker"
     curl -fsSL https://get.docker.com | sh
+    
 
     echoContent yellow " ---> 安装docker-compose"
-    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    curl -L "https://hub.gitmirror.com/https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 
 }
